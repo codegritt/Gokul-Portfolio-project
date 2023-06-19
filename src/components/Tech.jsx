7ea9009b9fc65a23251276216ad9c18b3608fa3a
gokul <gokulchandan18@gmail.com>
2023-06-19 13:51:11 +0530

    a

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,12 +3,23 @@ import { styles } from "../styles";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
     <>
       {" "}
+ 
+<motion.div 
+ 
+ variants={textVariant()}
+
+>
       <h2 className={`${styles.sectionHeadText}`}>Skills</h2>
+      </motion.div>
+
+      
       <div className="p-5 pl-0">
 
  <p className="text-lg font-bold">Web development :
@@ -54,6 +65,54 @@ const Tech = () => {
             <BallCanvas icon={technology.icon} />
           </div>
         ))}
+      </div>
+      <div >
+      <motion.div 
+ 
+ variants={textVariant()}
+
+>
+      <h2 className={`${styles.sectionHeadText}`}>Education</h2>
+  
+
+      <div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+     
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-xl">
+                   VIT University, Vellore
+                   B.Tech - EEE (6.74 / 10 CGPA)
+                </th>
+             
+                <td class="px-6 py-4 text-lg">
+             2012 - 2017 
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-xl">
+                  12th Grade 
+                  HSC (91.5%)
+                </th>
+             
+                <td class="px-6 py-4 text-lg" >
+                 2010 - 2012 
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-xl">
+                10th Grade 
+                  SSC (92.6%)
+                </th>
+             
+                <td class="px-6 py-4 text-lg">
+                2008 - 2010
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</motion.div>
       </div>
     </>
   );
