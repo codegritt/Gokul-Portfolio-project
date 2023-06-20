5be3246db9b489843cae4cb82bfbf2b625bf008c
gokul <gokulchandan18@gmail.com>
2023-06-20 22:27:43 +0530

    a

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import Profilepic from "./Profilepic";
 
 const Hero = () => {
   return (
@@ -31,6 +32,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
+      <Profilepic/>
 
       <ComputersCanvas />
 
